c2473c9382a7d93ecec08ef5e280d857d1e87784
Christopher <chrisobrien416@gmail.com>
2021-04-27 15:52:42 -0400

    implemented conditional rendering of checkboxes
    checkbox rows now change color when checked/ unchecked

--- src/Components/AllFiles/AllFiles.js
+++ src/Components/AllFiles/AllFiles.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
+//declar
+//each td will have a ternary operator className={checked.includes(index)}
+//when a box is checked, we pass the index to an onCheck function
+//the onCheck function will first see if checked includes the index
+//if it does not, push it
+//if it does, filter it
+
+//whenever
 
 const AllFiles = ({ fileItems }) => {
+  const [checked, setChecked] = useState([]);
+
+  const onCheck = (isChecked, index) => {
+    // console.log(checked, isChecked);
+    if (isChecked) {
+      let checkedItems = [...checked, index];
+      setChecked(checkedItems);
+    } else {
+      console.log("filtering");
+      let checkedItems = checked.filter((item) => item !== index);
+      console.log(checkedItems);
+      setChecked(checkedItems);
+    }
+  };
+
   const renderedItems =
     fileItems.length > 0
       ? fileItems.map((item, index) => {
           return (
-            <tr className="active">
+            <tr className={checked.includes(index) ? "activated" : ""}>
               <td>
-                <input type="checkbox" value={index} />
+                <input
+                  type="checkbox"
+                  value={index}
+                  onClick={(e) => onCheck(e.target.checked, index)}
+                />
               </td>
               <td>{item.name}</td>
               <td>{item.type}</td>
